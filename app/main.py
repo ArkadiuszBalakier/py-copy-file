ffb3a80bfca44d6ce473ecb54023daa737f2647d
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-10-08 13:12:58 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if command.startswith("cp "):
+        arguments_list = command.split()
+
+        if len(arguments_list) != 3:
+            return
+
+        file_name_to_copy = arguments_list[1]
+        copied_file_name = arguments_list[2]
+
+        if file_name_to_copy == copied_file_name:
+            return
+        try:
+            with open(file_name_to_copy, "r") as file, open(copied_file_name, "w") as copied_file:
+                copied_file.write(file.read())
+        except FileNotFoundError as e:
+            print(e)
